061e884c746a0641493ecb7d5b2c50475c3140ec
Krushna153 <krushna.shah@eronkan.com>
2021-01-11 20:11:28 +0530

    Util module functionalities

---
+++ Hands_on_NodeJS/Ch-5/util.js
@@ -0,0 +1,10 @@
+var util = require('util');
+util.log('Hello');
+
+var util = require('util')
+var a = {1: true, 2: false};
+console.log(util.inspect(a));
+
+//Output is as below
+//11 Jan 19:39:41 - Hello
+//{ '1': true, '2': false }
