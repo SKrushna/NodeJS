aa5c8c8925544463e97a08dbe34745236e24ef56
Krushna153 <krushna.shah@eronkan.com>
2021-01-11 17:58:39 +0530

    Server starts listening to an event and respond accordingly

---
+++ Hands_on_NodeJS/Ch-5/EventHandler.js
@@ -0,0 +1,47 @@
+/*
+//Program 1:
+var http = require('http');
+
+var server = http.createServer();
+
+server.listen('8080','localhost',function(){
+    console.log('Server started on port 8080');
+});
+
+//Output on the console: Server Started on port 8080
+//But the server is not responding
+*/ 
+
+/*
+//Program 2:
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    res.end('Server Works!');
+});
+
+server.listen('8080','localhost',function(){
+    console.log('Server started on port 8080');
+});
+
+//Output on the console: Server Started on port 8080
+//And the server has responded 'Server Works!'
+*/
+
+
+//Program 3:
+var http = require('http');
+var events = require('events');
+
+var eventEmitter = new EventEmitter();
+
+var server = http.createServer(function(req,res){
+    eventEmitter.emit('Krushna requested'); //Event Trigger
+    res.end('Server Works!');
+});
+
+eventEmitter.on('Krushna requested', function(){
+    console.log('A request has been done on the server.');
+}); //Event Listener
+
+server.listen('8080','localhost', )
