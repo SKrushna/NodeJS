1ef84377b18939614df5d67caecc15ed5b39e8c2
Krushna153 <krushna.shah@eronkan.com>
2021-01-11 17:25:10 +0530

    Access attributes in a function

---
+++ Hands_on_NodeJS/Ch-1/functionAttributes.js
@@ -0,0 +1,7 @@
+//This will generate an output 'abc' on the console
+
+var myFunction = function() {
+	console.log('Hello');
+};
+myFunction.someProperty = 'abc';
+console.log(myFunction.someProperty);
