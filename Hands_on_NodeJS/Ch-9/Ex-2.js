3c8bb245c34eeafb5e72659850dcbfeb9d2af4bc
SKrushna <krushna.shah@eronkan.com>
2021-01-12 20:38:13 +0530

    Exercise-2

---
+++ Hands_on_NodeJS/Ch-9/Ex-2.js
@@ -0,0 +1,19 @@
+//Having a file named a.txt, print bytes 10 to 14.
+module.exports = function(){
+    
+    var fs = require('fs');
+    fs.open('./a.txt', 'r', function(err, fd) {
+        if (err) { throw err }
+        var readBuffer = new Buffer(5),
+        bufferOffset = 0,
+        bufferLength = readBuffer.length,
+        filePosition = 9;
+        fs.read(fd, readBuffer, bufferOffset, bufferLength, filePosition, function(err, readBytes) {
+            if (err) { throw err; }
+            console.log('\njust read ' + readBytes + ' bytes\n');
+            if (readBytes > 0) {
+                console.log(readBuffer.slice(0, readBytes).toString());
+            }
+        });
+    });
+};
