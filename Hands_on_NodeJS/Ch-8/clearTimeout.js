c2f8cfe471f85644c2c91ebeea71fbde3ac19844
Krushna153 <krushna.shah@eronkan.com>
2021-01-12 11:47:31 +0530

    Clear timeout of an event

--- Hands_on_NodeJS/Ch-8/clearTimeout.js
+++ Hands_on_NodeJS/Ch-8/clearTimeout.js
@@ -1,3 +1,10 @@
+//Program 1: Here the timeout will never execute because we clear it right after we set it.
+
+var timeoutHandle = setTimeout(function() { console.log('yehaa!'); }, 1000);
+clearTimeout(timeoutHandle);
+
+//Program 2:
+
 var timeoutA = setTimeout(function() {
     console.log('timeout A');
 }, 2000);
@@ -5,9 +12,17 @@ var timeoutA = setTimeout(function() {
 var timeoutB = setTimeout(function() {
     console.log('timeout B');
     clearTimeout(timeoutA);
-});
+}, 1000);
 
-//process.nextTick
+
+//Program 3(Set Interval): Prints "tick" every 1 second
+var period = 1000; // 1 second
+var interval = setInterval(function() {
+console.log('tick');
+}, period);
+
+
+//Program 4(process.nextTick):
 process.nextTick(function() {
     // this runs on the next event loop
     console.log('yay!');
