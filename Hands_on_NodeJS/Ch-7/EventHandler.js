c3972ea2370e5bfd6aea8f3a6a4dde9cef45b886
Krushna153 <krushna.shah@eronkan.com>
2021-01-12 11:05:18 +0530

    Event Handler

--- Hands_on_NodeJS/Ch-7/EventHandler.js
+++ Hands_on_NodeJS/Ch-7/EventHandler.js
@@ -1,35 +1,3 @@
-/*
-//Program 1:
-var http = require('http');
-
-var server = http.createServer();
-
-server.listen('8080','localhost',function(){
-    console.log('Server started on port 8080');
-});
-
-//Output on the console: Server Started on port 8080
-//But the server is not responding
-*/ 
-
-/*
-//Program 2:
-var http = require('http');
-
-var server = http.createServer(function(req, res){
-    res.end('Server Works!');
-});
-
-server.listen('8080','localhost',function(){
-    console.log('Server started on port 8080');
-});
-
-//Output on the console: Server Started on port 8080
-//And the server has responded 'Server Works!'
-*/
-
-
-//Program 3: 
 //Whenever there is request put on the server(localhost), a message is displayed 
 //in the console: "A request has been done on the server."
 var http = require('http');
