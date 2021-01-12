f3540499c9adbd9ae403095d9b9e8ffcf2663025
SKrushna <krushna.shah@eronkan.com>
2021-01-12 20:46:47 +0530

    Print the meta info of a file

---
+++ Hands_on_NodeJS/Ch-9/fs_stat.js
@@ -0,0 +1,14 @@
+//this will print the meta info of the file from /etc/passwd. \
+//No need to manually create any folder or files to run this program, as every PC has this folder inbuilt
+module.exports = function(){
+    var fs = require('fs');
+
+    fs.stat('/etc/passwd', function(err, stats) {
+    if (err) {console.log(err.message); return; }
+        console.log(stats);
+        //This will print "this file is 2871 bytes long.""
+        console.log('this file is ' + stats.size + ' bytes long.');
+    });
+};
+//You can also create a sample.txt file having some data in it and then try to run this program 
+//by changing line no. 3 to fs.stat('./hello.txt', function(err, stats) {
