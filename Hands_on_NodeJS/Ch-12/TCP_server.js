ab1b4baa18c0edeef26e221226feae9f31e01329
SKrushna <krushna.shah@eronkan.com>
2021-01-13 20:37:27 +0530

    TCP server

---
+++ Hands_on_NodeJS/Ch-12/TCP_server.js
@@ -0,0 +1,30 @@
+module.exports = function(){
+    require('net').createServer(function(socket) {
+        // new connection
+        socket.on('data', function(data) {
+            // got data
+            console.log("data = "+data.toString());
+        });
+        socket.on('end', function(data) {
+            // connection closed
+            console.log("Connection closed");
+        });
+        socket.write('Some string');
+        
+        var port = 4001;
+        var host = '0.0.0.0';
+        var server = require('http').createServer();
+        server.listen(port, host, function() {
+            console.log('server listening on port ' + port);
+        });
+
+        socket.setTimeout(60000, function() {
+            socket.end('idle timeout, disconnecting, bye!');
+        });
+
+        server.close();
+        server.on('close', function() {
+            console.log('server closed!');
+        })
+    }).listen(4000);
+}
