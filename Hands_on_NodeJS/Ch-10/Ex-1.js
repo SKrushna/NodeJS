93d60c4bdb1dd4e733199afc8bef8fced98b0d15
SKrushna <krushna.shah@eronkan.com>
2021-01-13 12:31:02 +0530

    Exercise-1: Display content of static file on server by giving path name in url

--- Hands_on_NodeJS/Ch-10/Ex-1.js
+++ Hands_on_NodeJS/Ch-10/Ex-1.js
@@ -4,16 +4,16 @@ module.exports = function(){
     http = require('http');
 
     http.createServer(function (req, res) {
-    fs.readFile(__dirname + req.url, function (err,data) {
-        if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
-    }).listen(4000);
+        fs.readFile(__dirname + req.url, function (err,data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }).listen(4001);
 
     // var static = require('node-static');
     // var http = require('http');
