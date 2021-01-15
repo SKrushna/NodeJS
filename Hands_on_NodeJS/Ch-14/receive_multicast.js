838fe2afa1d96ba3e5386e4b743be17ee7a04c27
SKrushna <krushna.shah@eronkan.com>
2021-01-15 14:13:21 +0530

    Receiving multicast messages

---
+++ Hands_on_NodeJS/Ch-14/receive_multicast.js
@@ -0,0 +1,16 @@
+//Receiver receives the multicast message
+//write "echo 'hello' | netcat -u -w 1 239.0.0.0 4000 in another terminal and hello will be received here.
+var server = require('dgram').createSocket('udp4');
+
+server.on('message', function(message, rinfo) {
+    console.log('server got message: ' + message + ' from ' + rinfo.address + ':' + rinfo.port);
+});
+
+/*never do like this as the below commmented lines will generate an error*/
+// server.addMembership('230.1.2.3');
+// server.bind(4000);
+
+//always do like this
+server.bind(4000, function(){
+    server.addMembership('239.0.0.0');
+});
